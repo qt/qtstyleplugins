7ba68002216591c0ee969e26ad266d431f9b69c5
J-P Nurmi <jpnurmi@digia.com>
2012-10-24 19:42:03 +0200

    Restored QCDEStyle
    Change-Id: Ic09381943ebb92ec680073301739fc494d02fe5a
    Reviewed-by: Jens Bache-Wiig <jens.bache-wiig@digia.com>

--- src/plugins/styles/motif/motif.pro
+++ src/plugins/styles/motif/motif.pro
@@ -3,7 +3,9 @@ load(qt_plugin)
 
 QT = core gui widgets
 
+HEADERS += qcdestyle.h
 HEADERS += qmotifstyle.h
+SOURCES += qcdestyle.cpp
 SOURCES += qmotifstyle.cpp
 SOURCES += plugin.cpp
 
--- src/plugins/styles/motif/qcdestyle.h
+++ src/plugins/styles/motif/qcdestyle.h
@@ -3,7 +3,7 @@
 ** Copyright (C) 2012 Digia Plc and/or its subsidiary(-ies).
 ** Contact: http://www.qt-project.org/legal
 **
-** This file is part of the MNG plugins in the Qt ImageFormats module.
+** This file is part of the QtGui module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
 ** Commercial License Usage
@@ -39,30 +39,36 @@
 **
 ****************************************************************************/
 
-#include <QStylePlugin>
+
+#ifndef QCDESTYLE_H
+#define QCDESTYLE_H
+
 #include "qmotifstyle.h"
-#include "qcdestyle.h"
+
+QT_BEGIN_HEADER
 
 QT_BEGIN_NAMESPACE
 
-class QMotifStylePlugin : public QStylePlugin
+class Q_WIDGETS_EXPORT QCDEStyle : public QMotifStyle
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QStyleFactoryInterface" FILE "motif.json")
-
 public:
-    QStyle *create(const QString &key);
-};
+    explicit QCDEStyle(bool useHighlightCols = false);
+    virtual ~QCDEStyle();
 
-QStyle *QMotifStylePlugin::create(const QString &key)
-{
-    if (key == "motif")
-        return new QMotifStyle;
-    if (key == "cde")
-        return new QCDEStyle;
-    return 0;
-}
+    int pixelMetric(PixelMetric metric, const QStyleOption *option = 0,
+                    const QWidget *widget = 0) const;
+    void drawControl(ControlElement element, const QStyleOption *opt, QPainter *p,
+                     const QWidget *w = 0) const;
+    void drawPrimitive(PrimitiveElement pe, const QStyleOption *opt, QPainter *p,
+                       const QWidget *w = 0) const;
+    QPalette standardPalette() const;
+    QIcon standardIcon(StandardPixmap standardIcon, const QStyleOption *opt = 0,
+                       const QWidget *widget = 0) const;
+};
 
 QT_END_NAMESPACE
 
-#include "plugin.moc"
+QT_END_HEADER
+
+#endif // QCDESTYLE_H
